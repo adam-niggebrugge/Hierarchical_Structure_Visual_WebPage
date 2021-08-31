efb39543f9ef3ade3197e0a7594928917878a569
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-08-31 10:29:36 -0400

    revised all existing object scripts and test scripts to reflect object holding objects

--- main/__test__/Defender.test.js
+++ main/__test__/Defender.test.js
@@ -1,20 +1,22 @@
 const Defender = require("../lib/Defender");
+const TeamMember = require("../lib/TeamMember");
+
+const tm = new TeamMember("foo", "foo@foo.com");
 
 describe("Defender", () => {
     describe("Initialization Check", () => {
-        it("Can set averageHeaderAmt via constructor for TeamMember", () => {
+        it("Can set averageHeaderAmt via constructor", () => {
         const testValue = 100;
         const d = new Defender("Foo", 1, "test@test.com", testValue, 1);
         expect(d.averageHeaderAmt).toBe(testValue);
         });
-        it("Can set blocks via constructor for TeamMember", () => {
+        it("Can set blocks via constructor", () => {
             const testValue = 100;
             const d = new Defender("Foo", 1, "test@test.com", 1, testValue);
             expect(d.blocks).toBe(testValue);
         });
     }); 
         
-
     describe("getPosition()", () => {
         it("getPosition() should return \"Defender\"", () => {
         const testValue = "Defender";
--- main/__test__/Forward.test.js
+++ main/__test__/Forward.test.js
@@ -1,26 +1,21 @@
 const Forward = require("../lib/Forward");
+const TeamMember = require("../lib/TeamMember");
+
+const tm = new TeamMember("foo", "foo@foo.com");
 
 describe("Forward", () => {
-    describe(" TeamMemberInitialization", () => {
-        it("Can set scored goals via constructor for TeamMember", () => {
+    describe("Initialization", () => {
+        it("Can set scored goals via constructor", () => {
         const testValue = 100;
-        const f = new Forward("Foo", 1, "test@test.com", testValue);
+        const f = new Forward(1, "testName", tm, testValue);
         expect(f.scoredGoals).toBe(testValue);
         });
     }); 
-        
-    describe("Captain Initialization", () => {
-        it("Can set scored goals via constructor for Captain", () => {
-            const testValue = 100;
-            const f = new Forward("Foo", 1, "test@test.com", 1, testValue);
-            expect(f.scoredGoals).toBe(testValue);
-        });
-    });
 
     describe("getPosition()", () => {
         it("getPosition() should return \"Forward\"", () => {
         const testValue = "Forward";
-        const f = new Forward("Foo", 1, "test@test.com", 1);
+        const f = new Forward(1, "testName", tm, 99);
         expect(f.getPosition()).toBe(testValue);
         });
     });
@@ -28,7 +23,7 @@ describe("Forward", () => {
     describe("getGoals()", () => {
         it("Can get ScoredGoals via getGoals()", () => {
         const testValue = 100;
-        const f = new Forward("Foo", 1, "test@test.com", testValue);
+        const f = new Forward(1, "testName", tm, testValue);
         expect(f.getGoals()).toBe(testValue);
         });
     });
