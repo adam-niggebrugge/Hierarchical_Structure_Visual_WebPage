09c522a498e5088b7af9ee355a1a475e334797a6
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-08-31 00:23:10 -0400

    Did not complete variable name change in constructors

--- main/lib/Forward.js
+++ main/lib/Forward.js
@@ -3,12 +3,12 @@ const Captain = require("./Captain");
 class Forward extends Captain {
     constructor(name, kitNumber, email, option1, option2) {
         //check if last parameter is given, if option2 has a value then it must be a captain type person with experience for option1 and scoredGoals for option2
-        if(typeof scoredGoals !== 'undefined'){
-            super(name, kitNumber, email, experience);
-            this.scoredGoals = scoredGoals;
+        if(typeof option2 !== 'undefined'){
+            super(name, kitNumber, email, option1);
+            this.scoredGoals = option2;
         } else {
             super(name, kitNumber, email);
-            this.scoredGoals =  experience;
+            this.scoredGoals =  option1;
         } 
     }
 
