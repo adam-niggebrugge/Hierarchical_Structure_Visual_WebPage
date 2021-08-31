94edf472df66056aaecf7726a52bf132476b8259
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-08-31 18:24:16 -0400

    HTML template built out and linked to inquirer script

--- main/src/html-template.js
+++ main/src/html-template.js
@@ -5,23 +5,31 @@ const generateClub = club => {
     const generateForward = forward => {
         if(forward.getCharacter().getRole() === 'Captain'){
             return `
-                <div class="card employee-card">
+                <div class="card captain-card">
                     <div class="card-header">
-                        <h2 class="card-title">${forward.getName()}</h2>
-                        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${forward.getPosition()}</h3>
+                        <h2 class="card-title">${forward.getCharacter().getName()}
+                        </h2>
+                        <h3 class="card-title">
+                            <i class="fas fa-certificate mr-2"></i>${forward.getCharacter().getRole()}
+                        </h3>
+                        <h3 class="card-title"><i class="fas fa-king mr-2"></i>
+                            ${forward.getPosition()}
+                        </h3>
                     </div>
                     <div class="card-body">
                         <ul class="list-group">
-                            <li class="list-group-item">ID: ${forward.getKitNumber()}</li>
+                            <li class="list-group-item">Club Captain's Reign: ${forward.getCharacter().getExperience()}
+                            </li>
+                            <li class="list-group-item">Proudly Wears: ${forward.getKitNumber()}</li>
                             <li class="list-group-item">Email:      <a href="mailto:${forward.getCharacter().getEmail()}">${forward.getCharacter().getEmail()}
                                 </a>
                             </li>
-                            <li class="list-group-item">Club Captain's Reign: ${forward.getCharacter().getExperience()}
+                            <li  class="list-group-item">Tweet to ${forward.getTwitterHandle()}
+                                <a href="https://twitter.com/intent/tweet?screen_name=${forward.getTwitterHandle()}&ref_src=twsrc%5Etfw" class="twitter-mention-button" data-show-count="false">
+                                 </a>
                             </li>
-                            <li class="list-group-item">Tweet to ${forward.getTwitterHandle()}
-                            <a href="https://twitter.com/intent/tweet?screen_name=${forward.getTwitterHandle()}&ref_src=twsrc%5Etfw" class="twitter-mention-button" data-show-count="false">
-                            </a>
-                        </li>
+                            <li class="list-group-item">Goals Scored for Club : ${forward.getGoals()}
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -30,18 +38,20 @@ const generateClub = club => {
             return `
                 <div class="card club-member-card">
                     <div class="card-header">
-                        <h2 class="card-title">${forward.getName()}</h2>
-                        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${forward.getPosition()}</h3>
+                        <h2 class="card-title">${forward.getCharacter().getName()}</h2>
+                        <h3 class="card-title"><i class="fas fa-king mr-2"></i>${forward.getPosition()}</h3>
                     </div>
                     <div class="card-body">
                         <ul class="list-group">
-                            <li class="list-group-item">ID: ${forward.getKitNumber()}</li>
+                            <li class="list-group-item">Proudly Wears: ${forward.getKitNumber()}</li>
                             <li class="list-group-item">Email:      <a href="mailto:${forward.getCharacter().getEmail()}">${forward.getCharacter().getEmail()}
                                 </a>
                             </li>
                             <li class="list-group-item">Tweet to ${forward.getTwitterHandle()}
                                 <a href="https://twitter.com/intent/tweet?screen_name=${forward.getTwitterHandle()}&ref_src=twsrc%5Etfw" class="twitter-mention-button" data-show-count="false">
                                 </a>
+                            </li>
+                            <li class="list-group-item">Goals Scored for Club : ${forward.getGoals()}
                             </li>
                         </ul>
                     </div>
@@ -52,31 +62,190 @@ const generateClub = club => {
 
     // create the html for Defenders
     const generateDefender = defender => {
-        return `
-        <div class="card club-member-card">
-    <div class="card-header">
-        <h2 class="card-title">${defender.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${defender.getPosition()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${defender.getKitNumber()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${defender.getCharacter().getEmail()}">${defender.getCharacter().getEmail()}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/${defender.getGithub()}" target="_blank" rel="noopener noreferrer">${defender.getGithub()}</a></li>
-        </ul>
-    </div>
-</div>
-        `;
+        if(defender.getCharacter().getRole() === 'Captain'){
+            return `
+            <div class="card captain-card">
+                <div class="card-header">
+                    <h2 class="card-title">${defender.getCharacter().getName()}</h2>
+                    <h3 class="card-title">
+                        <i class="fas fa-certificate mr-2"></i>${defender.getCharacter().getRole()}
+                    </h3>
+                    <h3 class="card-title"><i class="fas fa-chess-rook mr-2"></i>${defender.getPosition()}</h3>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">Club Captain's Reign: ${defender.getCharacter().getExperience()}
+                        </li>
+                        <li class="list-group-item">Proudly Wears: ${defender.getKitNumber()}</li>
+                        <li class="list-group-item">Email:  
+                            <a href="mailto:${defender.getCharacter().getEmail()}">${defender.getCharacter().getEmail()}
+                            </a>
+                        </li>
+                        <li class="list-group-item">Tweet to ${defender.getTwitterHandle()}
+                                <a href="https://twitter.com/intent/tweet?screen_name=${defender.getTwitterHandle()}&ref_src=twsrc%5Etfw" class="twitter-mention-button" data-show-count="false">
+                                </a>
+                        </li>
+                        <li class="list-group-item">Average Header Per Match : ${defender.getAverageHeaderAmt()}
+                        </li>
+                        <li class="list-group-item">Blocks this Season : ${defender.getBlocks()}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            `;
+        } else {
+            return `
+            <div class="card club-member-card">
+                <div class="card-header">
+                    <h2 class="card-title">${defender.getCharacter().getName()}</h2>
+                    <h3 class="card-title"><i class="fas fa-chess-rook mr-2"></i>${defender.getPosition()}</h3>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">Proudly Wears: ${defender.getKitNumber()}</li>
+                        <li class="list-group-item">Email:  
+                            <a href="mailto:${defender.getCharacter().getEmail()}">${defender.getCharacter().getEmail()}
+                            </a>
+                        </li>
+                        <li class="list-group-item">Tweet to ${defender.getTwitterHandle()}
+                                <a href="https://twitter.com/intent/tweet?screen_name=${defender.getTwitterHandle()}&ref_src=twsrc%5Etfw" class="twitter-mention-button" data-show-count="false">
+                                </a>
+                        </li>
+                        <li class="list-group-item">Average Header Per Match : ${defender.getAverageHeaderAmt()}
+                        </li>
+                        <li class="list-group-item">Blocks this Season : ${defender.getBlocks()}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            `;
+        }
     };
 
     // create the html for midfielders
     const generateMidfield = midfield => {
-
+        if(midfield.getCharacter().getRole() === 'Captain'){
+            return `
+            <div class="card captain-card">
+                <div class="card-header">
+                    <h2 class="card-title">${midfield.getCharacter().getName()}</h2>
+                    <h3 class="card-title">
+                        <i class="fas fa-certificate mr-2"></i>${midfield.getCharacter().getRole()}
+                    </h3>
+                    <h3 class="card-title"><i class="fas fa-chess-knight mr-2"></i>${midfield.getPosition()}</h3>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">Club Captain's Reign: ${midfield.getCharacter().getExperience()}
+                        </li>
+                        <li class="list-group-item">Proudly Wears: ${midfield.getKitNumber()}</li>
+                        <li class="list-group-item">Email:  
+                            <a href="mailto:${midfield.getCharacter().getEmail()}">${midfield.getCharacter().getEmail()}
+                            </a>
+                        </li>
+                        <li class="list-group-item">Tweet to ${midfield.getTwitterHandle()}
+                                <a href="https://twitter.com/intent/tweet?screen_name=${midfield.getTwitterHandle()}&ref_src=twsrc%5Etfw" class="twitter-mention-button" data-show-count="false">
+                                </a>
+                        </li>
+                        <li class="list-group-item">Average Duals Won Per Match : ${midfield.getDualsWon()}
+                        </li>
+                        <li class="list-group-item">Longest Distance Goal Scored : ${midfield.getLongestGoalScored()}!!
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            `;
+        } else {
+            return `
+            <div class="card club-member-card">
+                <div class="card-header">
+                    <h2 class="card-title">${midfield.getCharacter().getName()}</h2>
+                    <h3 class="card-title"><i class="fas fa-chess-knight mr-2"></i>${midfield.getPosition()}</h3>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">Proudly Wears: ${midfield.getKitNumber()}</li>
+                        <li class="list-group-item">Email:  
+                            <a href="mailto:${midfield.getCharacter().getEmail()}">${midfield.getCharacter().getEmail()}
+                            </a>
+                        </li>
+                        <li class="list-group-item">Tweet to ${midfield.getTwitterHandle()}
+                                <a href="https://twitter.com/intent/tweet?screen_name=${midfield.getTwitterHandle()}&ref_src=twsrc%5Etfw" class="twitter-mention-button" data-show-count="false">
+                                </a>
+                        </li>
+                        <li class="list-group-item">Average Duals Won Per Match : ${midfield.getDualsWon()}
+                        </li>
+                        <li class="list-group-item">Longest Distance Goal Scored : ${midfield.getLongestGoalScored()}!!
+                        </li>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            `;
+        }
     };
 
-    // create the html for midfielders
+    // create the html for goalie
     const generateGoalie = goalie => {
-       
+        if(goalie.getCharacter().getRole() === 'Captain'){
+            return `
+            <div class="card captain-card">
+                <div class="card-header">
+                    <h2 class="card-title">${goalie.getCharacter().getName()}</h2>
+                    <h3 class="card-title">
+                        <i class="fas fa-certificate mr-2"></i>${goalie.getCharacter().getRole()}
+                    </h3>
+                    <h3 class="card-title"><i class="fas fa-chess-queen mr-2"></i>${goalie.getPosition()}</h3>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">Club Captain's Reign: ${goalie.getCharacter().getExperience()}
+                        </li>
+                        <li class="list-group-item">Proudly Wears: ${goalie.getKitNumber()}</li>
+                        <li class="list-group-item">Email:  
+                            <a href="mailto:${goalie.getCharacter().getEmail()}">${goalie.getCharacter().getEmail()}
+                            </a>
+                        </li>
+                        <li class="list-group-item">Tweet to ${goalie.getTwitterHandle()}
+                                <a href="https://twitter.com/intent/tweet?screen_name=${goalie.getTwitterHandle()}&ref_src=twsrc%5Etfw" class="twitter-mention-button" data-show-count="false">
+                                </a>
+                        </li>
+                        <li class="list-group-item">Total Clean Sheet : ${goalie.getCleanSheets()}
+                        </li>
+                        <li class="list-group-item">Saves this Season : ${goalie.getSaves()}!!
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            `;
+        } else {
+            return `
+            <div class="card club-member-card">
+                <div class="card-header">
+                    <h2 class="card-title">${goalie.getCharacter().getName()}</h2>
+                    <h3 class="card-title"><i class="fas fa-chess-queen mr-2"></i>${goalie.getPosition()}</h3>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">Proudly Wears: ${goalie.getKitNumber()}
+                        </li>
+                        <li class="list-group-item">Email:  
+                            <a href="mailto:${goalie.getCharacter().getEmail()}">${goalie.getCharacter().getEmail()}
+                            </a>
+                        </li>
+                        <li class="list-group-item">Tweet to ${goalie.getTwitterHandle()}
+                                <a href="https://twitter.com/intent/tweet?screen_name=${goalie.getTwitterHandle()}&ref_src=twsrc%5Etfw" class="twitter-mention-button" data-show-count="false">
+                                </a>
+                        </li>
+                        <li class="list-group-item">Total Clean Sheet : ${goalie.getCleanSheets()}
+                        </li>
+                        <li class="list-group-item">Saves this Season : ${goalie.getSaves()}!!
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            `;
+        }
     };
 
 
