e6bcc122d8a9f4607096be361d05ecabb8bc0db7
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-08-30 23:56:01 -0400

    revised variable naming and comment for overloaded function

--- main/lib/Forward.js
+++ main/lib/Forward.js
@@ -1,8 +1,8 @@
 const Captain = require("./Captain");
 
 class Forward extends Captain {
-    constructor(name, kitNumber, email, experience, scoredGoals) {
-        //check if last two parameters are given, if scoredGoals has a value then it must be a captain type person
+    constructor(name, kitNumber, email, option1, option2) {
+        //check if last parameter is given, if option2 has a value then it must be a captain type person with experience for option1 and scoredGoals for option2
         if(typeof scoredGoals !== 'undefined'){
             super(name, kitNumber, email, experience);
             this.scoredGoals = scoredGoals;
