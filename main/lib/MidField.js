d0994729b2f58f8d4a0866127576839168c9d14c
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-08-31 14:54:02 -0400

    Add comment to the constructor

--- main/lib/MidField.js
+++ main/lib/MidField.js
@@ -1,6 +1,14 @@
 const Player = require("./Player");
 
 class Midfield extends Player {
+    /**
+     * 
+     * @param {*} kitNumber expect postive number
+     * @param {*} twitterhandle expect string starting with @
+     * @param {*} object expecting either teamMember or captain object
+     * @param {*} longestGoalScored expecting postive number
+     * @param {*} dualsWon expecting postive number
+     */
     constructor(kitNumber, twitterhandle, object, longestGoalScored, dualsWon) {
         super(kitNumber, twitterhandle, object);
         this.longestGoalScored = longestGoalScored;
